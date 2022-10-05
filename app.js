069666f8bd79e43086a695354cb069a7137310cb
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2022-10-05 01:08:12 -0300

    Desafío

--- app.js
+++ app.js
@@ -3,28 +3,86 @@ const path = require('path');
 
 const nombreArchivo = 'productos.json';
 
-const obtenerProductos = () => {
-    const dato = fs.readFileSync(nombreArchivo, 'utf-8');
+const obtenerProductos = async () => {
+    const dato = await fs.promises.readFileSync(nombreArchivo, 'utf-8')
     return JSON.parse(dato);
+
 }
 
-const getAll = () => {
-    const productos = obtenerProductos();
-    return productos
-}
+const guardarProductos = async (productos) => {
+    const data = JSON.stringify(productos, null, '\t')
+    await fs.promises.writeFileSync(nombreArchivo, data)
+};
+  
 
-const getById = (idBuscado) => {
-    const productos = obtenerProductos();
 
-    const indice = productos.findIndex((unProducto) => unProducto.id === idBuscado);
+// FUNCION SAVE:
 
-    if (indice < 0) {
-        throw new Error ("El producto no existe")
+const save = async (data) => {
+
+    const productos = await obtenerProductos();
+
+    const nuevoProducto = {
+        title: data.title,
+        price: data.price,
+        id: productos.length === 0 ? 1 : productos[productos.lenth -1].id +1
     }
 
-    return productos(indice);
+    productos.push(nuevoProducto);
+
+    await guardarProductos(productos)
+
 }
 
-const resultado = getById(2);
+save({
+    title: "Memoria RAM DDR4 8GB Corsair",
+    price: 13200
+})
 
-console.log(resultado)
+// FUNCION GET ALL:
+
+const getAll = async () => {
+    const productos = await obtenerProductos();
+    return productos
+
+}
+
+getAll().then((data) => {
+    console.log(data)
+})
+
+
+// FUNCION GET BY ID:
+
+const getById = async (id) => {
+    const arrayFinal = await obtenerProductos();
+
+    const indice = arrayFinal.findIndex((product) => product.id === id);
+
+    if (indice < 0) {
+        throw new Error('El producto no existe')
+    }
+    return arrayFinal(indice)
+}
+
+// FUNCION DELETE ALL
+
+const deleteAll = async () => {
+    await guardarProductos([]);
+}
+
+// FUNCION DELETE BY ID
+
+const deleteById = async (id) => {
+    const productos = await obtenerProductos();
+
+    const indice = productos.findIndex((product) => product.id === id)
+
+    if(indice < 0){
+        return;
+    }
+
+    productos.splice(indice, 1);
+
+    await guardarProductos(productos)
+ }
