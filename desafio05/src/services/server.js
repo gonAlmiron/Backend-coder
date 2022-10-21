dc752cd8af67f0d83a7bfafe47b28b6cb44be3ee
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2022-10-21 16:22:04 -0300

    Commit funcionando

--- desafio05/src/services/server.js
+++ desafio05/src/services/server.js
@@ -1,27 +1,30 @@
 const express = require('express');
-const app = express();
+const { engine } = require('express-handlebars');
 const path = require('path');
-const {engine} = require ('express-handlebars');
+const app = express();
+
+
 
 app.use(express.static('public'));
 
+const viewsFolderPath = path.resolve(__dirname, '../../views');
+const layoutsFolderPath = `${viewsFolderPath}/layouts`;
+
+
 app.engine('hbs', engine({
     layoutsDir: layoutsFolderPath,
-	partialsDir: partialsFolderPath,
 	extname: 'hbs'
 }));
 app.set('view engine', 'hbs');
 app.set('views', './views');
 
-const viewsFolderPath = path.resolve(__dirname, '../../views');
-const layoutsFolderPath = `${viewsFolderPath}/layouts`;
 
-app.get('/', (req, res => {
-    res.json({
-        msg: "ok"
+
+app.get('/', (req, res) => {
+	res.json({
+		msg: 'ok'
+	})
 })
-}))
-
 
 
 
