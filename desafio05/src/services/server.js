59cbddd0655144dc427c8ec67432e1fede2f6799
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2022-10-26 16:11:43 -0300

    Commit

--- desafio05/src/services/server.js
+++ desafio05/src/services/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const { partials } = require('handlebars');
 const path = require('path');
 const app = express();
 
@@ -9,31 +10,54 @@ app.use(express.static('public'));
 
 const viewsFolderPath = path.resolve(__dirname, '../../views');
 const layoutsFolderPath = `${viewsFolderPath}/layouts`;
-
+const partialsFolderPath = `${viewsFolderPath}/partials`;
+const defaultLayoutPath = `${layoutsFolderPath}/index.hbs`;
 
 app.set('view engine', 'hbs');
 app.set('views', viewsFolderPath);
 
 app.engine('hbs', engine({
     layoutsDir: layoutsFolderPath,
-	extname: 'hbs'
+	extname: 'hbs',
+	defaultLayout: defaultLayoutPath,
+	partialsDir: partialsFolderPath
 }));
 
 
-
+// nombre: poner un ID al input form y luego va el INPUTFORM1.value
+// despues hay que hacer un += para sumarlo a la lista
+// armar una base de la lista
 app.get('/test', (req, res) => {
+	const objetoConDataDinamica = {
+		nombre: 'Carlitos',
+		apellido: 'Tevez'
+	}
 	res.json({
 		msg: 'ok'
 	})
 })
 
 app.get('/', (req, res) => {
-	res.render('main', {layout: 'index'})
+	const objetoConDataDinamica = {
+		nombre: "Ricardo",
+		apellido: "Bochini",
+		productos: ["Mate", "Cafe", "Harina","Palmitos"],
+		productoss: [{
+			nombre: 'mate', estilo: 'toplaner',
+			nombre: 'cafe', estilo: 'midlaner',
+			nombre: 'harina', estilo: 'toplaner',
+			nombre: 'palmitos', estilo: 'midlaner',
+		}],
+		mostrarProductos: true
+	};
+	res.render('main', objetoConDataDinamica)
 })
 
 app.get('/productos', (req, res) => {
 	res.render('productos', {layout: 'index'})
 })
+
+
 
 
 
