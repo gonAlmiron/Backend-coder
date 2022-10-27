966cfb041a63091032af92f416b0ef1c8192c7a7
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2022-10-27 12:57:08 -0300

    Commit

--- websockets/public/js/cliente.js
+++ websockets/public/js/cliente.js
@@ -1,2 +1,24 @@
 const socket = io.connect();
 console.log("HOLAAAA")
+
+const form = document.getElementById('formulario');
+const author = document.getElementById('username');
+const text = document.getElementById('texto');
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault(); // esto es clave para evitar que el form quede en blanco
+
+    console.log('Se ejecuto el submit')
+
+
+    const cartaParaElServer = {
+        author: author.value,
+        text: text.value,
+    }
+
+    console.log(cartaParaElServer)
+
+    author.value = '';
+    text.value = ''; // CON ESTO RESETEAMOS EL INPUT LUEGO DEL SUBMIT
+
+})
