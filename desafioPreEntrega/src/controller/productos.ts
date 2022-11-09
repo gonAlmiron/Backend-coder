a0472be6ad33ade0d7e5a5f2be6f096b935f3c53
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2022-11-09 15:27:56 -0300

    asd

--- desafioPreEntrega/src/controller/productos.ts
+++ desafioPreEntrega/src/controller/productos.ts
@@ -2,7 +2,7 @@
 
 // const createError = require('http-errors')
 
-// const createError = require('http-errors')
+
 
 // class ProductosAPI {
 //     constructor() {
--- desafioPreEntrega/src/routes/index.ts
+++ desafioPreEntrega/src/routes/index.ts
@@ -1,7 +1,5 @@
 import {Router} from 'express';
-
 import productosRouter from './productos';
-
 
 const router = Router();
 
