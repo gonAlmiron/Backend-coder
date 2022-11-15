76312b619d427a5634a8416589f47d0b5c019014
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2022-11-15 00:54:16 -0300

    sql

---
+++ SQL/options/mysql.js
@@ -0,0 +1,14 @@
+const options = {
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        port: 3307,
+        password: '',
+        database: 'eCommerce'
+    },
+};
+
+module.exports = {
+    options
+}
---
+++ SQL/src/createTable.js
@@ -0,0 +1,18 @@
+const {options} = require('../options/mysql');
+
+const knex = require('knex')(options);
+
+knex.schema
+  .createTable('cars', (table) => {
+    table.increments('id');
+    table.string('name');
+    table.integer('price');
+  })
+  .then(() => console.log('Table created!'))
+  .catch((err) => {
+    console.log('There was an error creating table cars');
+    console.log(err);
+  })
+  .finally(() => {
+    knex.destroy();
+  });
