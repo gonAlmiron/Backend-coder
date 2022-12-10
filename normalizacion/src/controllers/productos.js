b5f76a8b779032288d75a8260a7cc50f22d616e7
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2022-12-10 11:46:05 -0300

    d

---
+++ normalizacion/src/controllers/productos.js
@@ -0,0 +1,15 @@
+
+
+export const getAllProducts = async (req, res) => {
+
+}
+
+export const createMessage = async (req, res) => {
+
+}
+
+export const getAllMessages = async (req, res) => {
+
+
+}
+
--- normalizacion/src/routes/index.js
+++ normalizacion/src/routes/index.js
@@ -1,8 +1,18 @@
-import {application, Router} from 'express';
+import {Router} from 'express';
+import {
+    getAllProducts,
+    getAllMessages,
+    createMessage
+} from '../controllers/productos'
 
 
  const router = Router()
 
- 
+ router.get('/products', getAllProducts);
+
+ router.get('/messages', getAllMessages);
+
+ router.post('newMessage', createMessage);
+
 
  export default router
