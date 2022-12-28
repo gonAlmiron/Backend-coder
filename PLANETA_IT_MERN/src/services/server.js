89e10a2e644c0c3d465db78ed051b5746bec8d4f
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2022-12-28 18:57:35 -0300

    dd

--- PLANETA_IT_MERN/src/services/server.js
+++ PLANETA_IT_MERN/src/services/server.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import path from 'path'
 
 const app = express();
+
+// Middlewares
+app.use(express.json());
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));;
 
 
 export default app
