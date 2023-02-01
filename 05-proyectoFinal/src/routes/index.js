2eb8c33c4c8d68e82574209c54a1efd8dbb01400
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2023-02-01 19:58:54 -0300

    whatsapp

--- 05-proyectoFinal/src/routes/index.js
+++ 05-proyectoFinal/src/routes/index.js
@@ -3,6 +3,8 @@ import passport from 'passport';
 import log4js from 'log4js';
 import { transporter } from "../services/email.js";
 import { templateHtml } from '../services/template.js';
+import { twilioClient } from "../services/whatsapp.js";
+
 
 const router = Router();
 
@@ -79,6 +81,37 @@ router.post(
         console.log(error);
     }
 })
+
+
+router.post('/whatsapp', async(req, res) => {
+  try {
+    const message = {
+      body: req.body.message,
+      from: process.env.CEL,
+      to: req.body.dest,
+      mediaUrl: ['https://cadenaser.com/resizer/c09Az9WzwQFwSZPN90pP1dhNqQ8=/736x552/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/prisaradio/TOLWBLP2DRFWZPVWKRWIQ4WH3I.jpg']
+    };
+    const response = await twilioClient.messages.create(message);
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+  }
+}); 
+
+
+router.post('/inbox', async (req, res) => {
+  try {
+    console.log(req.body);
+    if(req.body.Body.toUpperCase().includes('HOLA')) {
+      await sendMessageToClient(req.body.WaId, `Hola ${req.body.ProfileName}!, ¿Cual es tu consulta?`)
+    }
+    if(req.body.Body.toUpperCase().includes('CHAU')) {
+      await sendMessageToClient(req.body.WaId, `Chau ${req.body.ProfileName}!, ¡Hasta pronto!`)
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}); 
 
 
 
