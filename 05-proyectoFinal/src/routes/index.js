1f00e902fcbd527050378e2b078d2fab38941b90
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2023-02-01 12:50:44 -0300

    incorporando EMAIL

--- 05-proyectoFinal/src/routes/index.js
+++ 05-proyectoFinal/src/routes/index.js
@@ -1,6 +1,8 @@
 import {Router} from 'express';
 import passport from 'passport';
 import log4js from 'log4js';
+import { transporter } from "../services/email.js";
+import { templateHtml } from '../services/template.js';
 
 const router = Router();
 
@@ -52,6 +54,31 @@ router.post(
     },
   );
 
+
+  router.post('/gmail', async(req, res) => {
+    const { dest } = req.body;
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: dest,
+        subject: '¡Bienvenido a la comisión 32155!',
+        // text: 'Hola, te damos la bienvenida a nuestra comisión de backend.'
+        // html: '<h1>Hola, te damos la bienvenida a nuestra comisión de backend.</h1>'
+        html: templateHtml,
+        attachments: [
+            {
+                path: process.cwd() + '/src/services/texto.txt',
+                filename: 'texto-adjunto-32155'
+            }
+        ]
+    };
+    try{
+        const response = await transporter.sendMail(mailOptions);
+        console.log('Email enviado!');
+        res.json(response);
+    }catch(error){
+        console.log(error);
+    }
+})
 
 
 
