6e8178b165d675f4a9425eb4bc3d04ca59223a35
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2023-02-08 19:58:52 -0300

    DD

--- 05-proyectoFinal/src/models/user.js
+++ 05-proyectoFinal/src/models/user.js
@@ -3,6 +3,10 @@ import { Schema, model } from 'mongoose';
 const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-});
+},
+{
+  versionKey: false, timestamps: true }
+)
+
 
 export const UserModel = model('user', UserSchema);
--- 05-proyectoFinal/src/services/auth.js
+++ 05-proyectoFinal/src/services/auth.js
@@ -1,7 +1,6 @@
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
 import { UserModel } from '../models/user';
-import log4js from 'log4js';
 import logger from './logger'
 
 
